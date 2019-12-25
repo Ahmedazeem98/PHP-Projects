a5a06891eb5526259c3b87b8fb179501f5d80606
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2019-12-26 01:09:35 +0200

    Add files via upload

---
+++ Gaming Store/includes/footer.php
@@ -0,0 +1,18 @@
+</div>
+              </div>
+
+        </div>
+
+      </div>
+
+    </div><!-- /.container -->
+
+    <div class="row footer">
+      <div class="container">
+        <p>The Gaming Store &copy; 2019, All Rights Reserved</p>
+      </div>
+    </div>
+
+
+  </body>
+</html>
